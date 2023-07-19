6887c6593cc5a0a795400e40269ff31924ba3954
Kevin Salinas <kevinsalinas155@gmail.com>
2023-07-19 01:58:49 -0400

    add name, email, password input fields

--- client/src/routes/profile/+page.svelte
+++ client/src/routes/profile/+page.svelte
@@ -1,17 +1,43 @@
 <script>
 
+    let profileImage = 'https://t4.ftcdn.net/jpg/04/83/90/95/360_F_483909569_OI4LKNeFgHwvvVju60fejLd9gj43dIcd.jpg'
 
 </script>
 
+<main>
+    <h1>
+        Welcome User
+    </h1>
 
-<h1>
-    Welcome User
-</h1>
+    <img src={profileImage} alt="profile">
+    <form action="profile">
+        <label for="fname">First Name:</label>
+        <input type="text" id='fname' name='fname'><br><br>
+        <label for="lname">Last Name:</label>
+        <input type="text" id='lname' name='lname'><br><br>
+        <label for="email">Email:</label>
+        <input type="text" id='email' name='email'><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id='password' name='password'><br><br>
+        <!-- I want to do this in react, using state to display form only when edit profile button is clicked -->
+        <button>Edit profile</button>
+        <button>Save</button>
+    </form>
+</main>
 
 
 <style>
     h1 {
         font-size: 2em;
+    }
+
+    img {
+        flex: auto;
+    }
+
+    button {
+
+        flex: auto;
     }
 </style>
 
