1fe8047bdaada15994dc2a89bbcd56029fdfbf87
Kevin Salinas <kevinsalinas155@gmail.com>
2023-07-20 23:59:18 -0400

    add profile basic functionality

--- client/src/routes/profile/+page.svelte
+++ client/src/routes/profile/+page.svelte
@@ -1,29 +1,36 @@
-<script>
+<!-- confirmed: this project uses svelte material ui @smui -->
 
-    let profileImage = 'https://t4.ftcdn.net/jpg/04/83/90/95/360_F_483909569_OI4LKNeFgHwvvVju60fejLd9gj43dIcd.jpg'
+<script>
+    import Form from './Form.svelte'
+    import profile_icon from "./assets/profile_icon.jpg"
+
+
+    let toggle = false;
+
+    function toggleProfile() {
+        toggle = !toggle
+    }
 
 </script>
 
-<main>
+<div class="container">
     <h1>
         Welcome User
     </h1>
+    <div>
+        <img src={profile_icon} alt="profile_image">
+    </div>
 
-    <img src={profileImage} alt="profile">
-    <form action="profile">
-        <label for="fname">First Name:</label>
-        <input type="text" id='fname' name='fname'><br><br>
-        <label for="lname">Last Name:</label>
-        <input type="text" id='lname' name='lname'><br><br>
-        <label for="email">Email:</label>
-        <input type="text" id='email' name='email'><br><br>
-        <label for="password">Password:</label>
-        <input type="password" id='password' name='password'><br><br>
-        <!-- I want to do this in react, using state to display form only when edit profile button is clicked -->
-        <button>Edit profile</button>
-        <button>Save</button>
-    </form>
-</main>
+    <button on:click={toggleProfile}>Edit profile</button>
+        
+    {#if toggle}
+       <div>
+            <Form/>
+            <button on:click={toggleProfile}>Save</button>
+        </div>
+    {/if}
+    
+</div>
 
 <!-- testing git contribution -->
 
---
+++ client/src/routes/profile/Form.svelte
@@ -0,0 +1,11 @@
+<form action="profile">
+    <label for="fname">First Name:</label>
+    <input type="text" id='fname' name='fname'><br><br>
+    <label for="lname">Last Name:</label>
+    <input type="text" id='lname' name='lname'><br><br>
+    <label for="email">Email:</label>
+    <input type="text" id='email' name='email'><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id='password' name='password'><br><br>
+    <!-- I want to do this in react, using state to display form only when edit profile button is clicked -->
+</form>
